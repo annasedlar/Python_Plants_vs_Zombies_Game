d6d2df5e1cc4a12f63d2130315e5fee5abb16cca
Anna Sedlar <annasedlar@gmail.com>
2017-02-28 15:59:05 -0500

    zombies pick random rows

--- zombie.py
+++ zombie.py
@@ -1,6 +1,6 @@
 import pygame; 
 from pygame.sprite import Sprite; 
-
+import random;
 
 class Zombie(Sprite): 
 	# this is the barebones of a sprite class in python
@@ -13,10 +13,28 @@ class Zombie(Sprite):
 		self.rect = self.image.get_rect(); 
 		self.screen = screen; 
 		self.screen_rect = screen.get_rect(); 
-		self.rect.centery = self.screen_rect.centery; 
+		# self.rect.centery = self.screen_rect.centery; 
 		self.rect.right = self.screen_rect.right; 
 
 		self.x = float(self.rect.x)
+
+		# find a random row (1-5) 
+		randy = random.randint(1,5);
+		if randy == 1:
+			self.rect.centery = 245
+		elif randy == 2:
+			self.rect.centery = 350
+		elif randy == 3:
+			self.rect.centery = 455
+		elif randy == 4:
+			self.rect.centery = 560
+		elif randy == 5:
+			self.rect.centery = 665
+
+
+
+
+
 
 	def update_me(self):
 		self.x -= self.speed * 1; 
